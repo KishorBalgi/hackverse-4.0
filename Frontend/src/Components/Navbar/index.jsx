477bdcd562d2723579ac45f599f6f1699057c480
Maniprasad Kadlaskar <maniprasadvkadlaskar@gmail.com>
2023-04-15 23:08:21 +0530

    auth config

--- Frontend/src/Components/Navbar/index.jsx
+++ Frontend/src/Components/Navbar/index.jsx
@@ -1,126 +1,153 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../Config';
 
 const Navbar = () => {
-  return (
-    <nav className="bg-gray-800">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-        <div className="relative flex items-center justify-between h-16">
-          {/* Logo */}
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0">
-              {/* <img
+    const [authenticated , setAuthenticated] = useState(false);
+
+    useEffect(() => {
+        api.get('/api/auth/isLoggedIn')
+            .then(res => {
+                if (res.success)
+                    setAuthenticated(true);
+            })
+            .catch(err => {
+                console.log(err.message);
+            })
+    },[])
+
+    const logoutHandler = () => {
+        api.get("/api/auth/logout")
+        .then(res => console.log("User logged out"))
+        .catch(err => console.log(err.message))
+
+        console.log("hey done");
+    }
+
+    return (
+        <nav className="bg-gray-800">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-16">
+                    {/* Logo */}
+                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex-shrink-0">
+                            {/* <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                 alt="Workflow"
               /> */}
+                        </div>
+                        <div className="hidden sm:block sm:ml-6">
+                            <div className="flex space-x-4">
+                                <Link
+                                    to="/"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                >
+                                    Dashboard
+                                </Link>
+
+                                <Link
+                                    to="#"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                >
+                                    Team
+                                </Link>
+
+                                <Link
+                                    to="#"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                >
+                                    Projects
+                                </Link>
+
+                                <Link
+                                    to="#"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                >
+                                    Calendar
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* User menu */}
+                    <div className="hidden sm:block sm:ml-6">
+                        <div className="flex space-x-4">
+                            {!authenticated ? (
+                            <>
+                            <Link
+                                to="/signin"
+                                className="bg-indigo-500 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Sign in
+                            </Link>
+                            <Link
+                                to="signup"
+                                className="bg-indigo-500 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Sign up
+                            </Link>
+                            </>) : 
+                            <button
+                                onClick={logoutHandler}
+                                className="bg-indigo-500 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Logout
+                            </button>}
+                        </div>
+                    </div>
+
+                    {/* Mobile menu */}
+                    <div className="-mr-2 flex items-center sm:hidden">
+                        <button
+                            type="button"
+                            className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                            aria-controls="mobile-menu"
+                            aria-expanded="false"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            {/* Icon when menu is closed. */}
+                            {/* Menu open: "hidden", Menu closed: "block" */}
+                            <svg
+                                className="block h-6 w-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                            </svg>
+                            {/* Icon when menu is open. */}
+                            {/* Menu open: "block", Menu closed: "hidden" */}
+                            <svg
+                                className="hidden h-6 w-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="hidden sm:block sm:ml-6">
-              <div className="flex space-x-4">
-                <Link
-                  to="/"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Dashboard
-                </Link>
-
-                <Link
-                  to="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Team
-                </Link>
-
-                <Link
-                  to="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Projects
-                </Link>
-
-                <Link
-                  to="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Calendar
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          {/* User menu */}
-          <div className="hidden sm:block sm:ml-6">
-            <div className="flex space-x-4">
-              <Link
-                to="/signin"
-                className="bg-indigo-500 text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Sign in
-              </Link>
-              <Link
-                to="signup"
-                className="bg-indigo-500 text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                Sign up
-              </Link>
-              <Link
-                to="otp"
-                className="bg-indigo-500 text-white px-3 py-2 rounded-md text-sm font-medium"
-              >
-                With OTP
-              </Link>
-            </div>
-          </div>
-
-          {/* Mobile menu */}
-          <div className="-mr-2 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              {/* Icon when menu is closed. */}
-              {/* Menu open: "hidden", Menu closed: "block" */}
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              {/* Icon when menu is open. */}
-              {/* Menu open: "block", Menu closed: "hidden" */}
-              <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-      </nav>
-  );
+        </nav>
+    );
 }
 
 export default Navbar;
