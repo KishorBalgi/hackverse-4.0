8be79c02aee590f4f303f57793b4e87b2ac9db28
Kishor Balgi <kishorbalgi@gmail.com>
2023-04-16 11:16:45 +0530

    CORS:fix

--- Backend/scripts/express.script.js
+++ Backend/scripts/express.script.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing files
 // Cors:
+// app.use(cors({ origin: meta.dev_host, credentials: true }));
 app.use(cors({ origin: meta.host, credentials: true }));
 
 app.listen(PORT, () => {
