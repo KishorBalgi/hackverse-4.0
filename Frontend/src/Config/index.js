109e5c2a1ec98ad2133d496dab63bd71c5af8602
Kishor Balgi <kishorbalgi@gmail.com>
2023-04-16 11:27:46 +0530

    fix: CORS

--- Frontend/src/Config/index.js
+++ Frontend/src/Config/index.js
@@ -3,10 +3,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASEURL,
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
   withCredentials: true,
 });
 
